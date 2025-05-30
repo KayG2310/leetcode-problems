73e41dd425d90bd542640c456ab99d64c61677d7
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-05-30 17:32:52 +0530

    sliding window approach, tc = 2n, sc = 1

---
+++ medium/209. Min Size Subarray sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int i=0, j=0, sum = 0, ans=INT_MAX;
+        while(i<=j && i<n && j<n){
+            sum += nums[j];
+            while(sum>=target){
+                ans = min(ans, j-i+1);
+                sum -= nums[i];
+                i++;
+            }
+            j++;
+        }
+        return (ans==INT_MAX) ? 0 : ans;
+    }
+};
