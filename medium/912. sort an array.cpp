a1cb910f045341e71d373e367df78fdf89558bc8
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-05-31 14:22:17 +0530

    tc = nlogn, sc = n

---
+++ medium/912. sort an array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>>pq;
+        for(int i: nums) pq.push(i);
+        vector<int>ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
