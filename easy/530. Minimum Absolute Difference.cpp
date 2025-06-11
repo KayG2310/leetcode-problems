280d59aeaa751c48dce3df4e4792467a9fc4100d
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-06-11 12:25:24 +0530

    space optimised

--- easy/530. Minimum Absolute Difference.cpp
+++ easy/530. Minimum Absolute Difference.cpp
@@ -29,3 +29,40 @@ public:
         return a;
     }
 };
+
+
+// constant space 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void inorder(TreeNode* root, int &ans, int &a){
+        if(root==NULL) return;
+        inorder(root->left, ans, a);
+        if(ans==1e9){
+            ans = root->val;
+        }
+        else{
+            a = min(a, abs(root->val-ans));
+            ans = root->val;
+        }
+        inorder(root->right, ans, a);
+    }
+public:
+    int getMinimumDifference(TreeNode* root) {
+        if(root==NULL) return 0;
+        int ans=1e9;
+        int a = 1e9;
+        inorder(root, ans, a);
+        return a;
+    }
+};
