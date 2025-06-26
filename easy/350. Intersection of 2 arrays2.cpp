e7d9005c3da22e03687a0c06920a0ca920bac0e6
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-06-26 20:27:06 +0530

    slight modification in lc 349

---
+++ easy/350. Intersection of 2 arrays2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int n = nums1.size(), m = nums2.size();
+        int i=0, j=0;
+        while(i<n && j<m){
+            while(i<n && nums1[i]<nums2[j])i++;
+            while(i<n && j<m && nums2[j]<nums1[i])j++;
+            if(i==n || j==m) break;
+            if(i<n && j<m && nums1[i]==nums2[j]) {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(i<n && j<m && nums1[i]<=nums2[j]) i++;
+            else j++;
+        }
+        return ans;
+    }
+};
