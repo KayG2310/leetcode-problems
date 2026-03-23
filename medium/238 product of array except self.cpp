f89486f3b100a2ccd89228ce8cf91cca93ab0e51
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-03-24 01:13:11 +0530

    brute--> better --> optimal

--- medium/238 product of array except self.cpp
+++ medium/238 product of array except self.cpp
@@ -1,28 +1,60 @@
+// brute
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        int product = 1, cntzero = 0, actual_product = 1;
-        vector<int>ans(n, 0);
+        vector<int> output;
         for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                cntzero++;
+            int product = 1;
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                product *= nums[j];
             }
-            else{
-                product *= nums[i];
-                // actual_product *= nums[i];
-            }
+            output.push_back(product);
         }
-        if(cntzero == n || cntzero > 1) return ans;
+        return output;
+    }
+};
+
+// 2 pre and suffix 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        vector<int> left_Product(n);
+        vector<int> right_Product(n);
+        left_Product[0] = 1;
+        for(int i=1; i<n; i++){
+            left_Product[i] = left_Product[i-1] * nums[i-1];
+        }
+        right_Product[n-1] = 1;
+        for(int i=n-2; i>=0; i--){
+            right_Product[i] = right_Product[i+1] * nums[i+1];
+        }
         for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                ans[i] = product;
-            }
-            else{
-                if(cntzero > 0) ans[i] = 0;
-                else ans[i] = product/nums[i];
-            }
+            ans[i] = left_Product[i] * right_Product[i];
         }
         return ans;
+    }
+};
+
+// reusing output array --> while going left to right, skip 1st while doing preproduct. then for last element, keep it as it is, store
+// int temp = 1 update it to last element so on
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n);
+        output[0] = 1;
+        for(int i=1; i<n; i++){
+            output[i] = output[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
     }
 };
